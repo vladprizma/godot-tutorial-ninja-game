c61a3fa0970da52a81e0013ca698479858548590
vladprizma <78017035+vladprizma@users.noreply.github.com>
2024-02-01 17:38:17 +0000

    Deploying to gh-pages from @ vladprizma/godot-tutorial-ninja-game@1c572009840de7f672399b142413f0bf7b972a14 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1705852473|3666717";
+const CACHE_VERSION = "1706809086|3867786";
 const CACHE_PREFIX = "2d Topdown Shoot-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
